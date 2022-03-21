1a4ed28d1bb6a9c4ce257280ed6c4ae39dc2cfd0
Golap-mostofa <mostofa6410@gmail.com>
2022-03-21 22:12:13 +0600

    header-components

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,9 +1,19 @@
 import React from 'react';
+import logo from '../../images/Logo.svg'
+import './Header.css'
 
 const Header = () => {
     return (
         <div>
-            <h2>ng</h2>
+           <nav className='header'>
+               <img src={logo} alt="" />
+               <div>
+                   <a href="/shop">shop</a>
+                   <a href="/orders">orders</a>
+                   <a href="/inventory">inventory</a>
+                   <a href="/about">about</a>
+               </div>
+           </nav>
         </div>
     );
 };
