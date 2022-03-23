a82b22363680fa65065646e2fb815c30c4e3fe7d
Golap-mostofa <mostofa6410@gmail.com>
2022-03-23 18:50:57 +0600

    localstoreg-data-set

---
+++ src/components/Card/Card.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import './Card.css'
+const Card = (props) => {
+        const {card} = props
+    console.log(card);
+        let total  = 0
+        let shipping = 0;
+        let quantity = 0;
+        for(const product of card){
+            quantity = quantity + product.quantity
+            total = total + product.price * product.quantity;
+            shipping = shipping +product.shipping;
+        }
+        const tax = parseFloat((total * 0.1).toFixed(2))
+      const  grandTotal = total + shipping + tax
+    return (
+        <div className='cart'>
+              <h4>Order new</h4>
+                <p>Item: {quantity}</p>
+                <p> price: ${total}</p>
+                <p>shipping: ${shipping}</p>
+                <p>tax: {tax}</p>
+                <h5>Total: {grandTotal.toFixed(2)}</h5>
+        </div>
+    );
+};
+
+export default Card;
--- src/components/Header/Shop/Shop.js
+++ src/components/Header/Shop/Shop.js
@@ -1,4 +1,6 @@
 import React, { useEffect, useState } from 'react';
+import { addToDb, getStordcart } from '../../../utilities/fakedb';
+import Card from '../../Card/Card';
 import Product from '../../Product/Product';
 import './Shop.css'
 
@@ -7,16 +9,51 @@ const Shop = () => {
     const [cart, setCart] = useState([]);
      
      useEffect(()=>{
+         console.log("face data");
     fetch('products.json')
     .then(res=>res.json())
-     .then(data=>setproducts(data))
+     .then(data=>{
+         setproducts(data)
+        //  console.log('object');
+        })
 
     },[])
 
-    const hendelAddTocart=(product)=>{
-        console.log(product);
-        const newcart = [...cart, product]
-        setCart(newcart)
+    useEffect(()=>{
+        console.log('localst',products);
+        const savedCart = []
+        const storedCart = getStordcart()
+        console.log(storedCart);
+        for(const id in storedCart){
+            const addProduct = products.find(product =>product.id ===id)
+            if(addProduct){
+                const quantity = storedCart[id]
+                addProduct.quantity = quantity
+                savedCart.push(addProduct)
+            }
+             
+        }
+        setCart(savedCart)
+        // console.log('localstoresfece');
+    },[products])
+
+    const hendelAddTocart=(selecetedproduct)=>{
+        console.log(selecetedproduct);
+        let newCart = []
+        const existe = cart.find(prodact => prodact.id === selecetedproduct.id)
+        if(!existe){
+            selecetedproduct.quantity = 1
+            newCart = [...cart, selecetedproduct]
+        }else{
+            const rest = cart.filter(product=>product.id !== selecetedproduct.id)
+            existe.quantity = existe.quantity +1
+            newCart = [...rest,existe]
+        }
+        
+       
+        setCart(newCart)
+        addToDb(selecetedproduct.id)
+       
     }
 
     return (
@@ -31,8 +68,7 @@ const Shop = () => {
                }
             </div>
             <div className="card-container">
-                <h4>samary</h4>
-                <p>sicleted: {cart.length}</p>
+              <Card card={cart}></Card>
             </div>
         </div>
     );
