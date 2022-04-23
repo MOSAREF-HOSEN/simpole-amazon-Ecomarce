d12f5fb39b51a1a4279260858e74392d8069432e
Golap-mostofa <mostofa6410@gmail.com>
2022-04-23 22:27:55 +0600

    create components

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import Header from './components/Header/Header.js';
 import Shop from './components/Header/Shop/Shop';
+import Inventory from './components/Inventory/Inventory';
+import Orders from './components/Orders/Orders';
 
 function App() {
   return (
     <div>
      <Header></Header>
-     <Shop></Shop>
+     <Routes>
+       <Route path='/' element={<Shop></Shop>}></Route>
+       <Route path='/shop' element={<Shop></Shop>}></Route>
+       <Route path='/orders' element={<Orders></Orders>}></Route>
+       <Route path='/inventory' element={<Inventory></Inventory>}></Route>
+     </Routes>
+     {/* <Shop></Shop> */}
     </div>
   );
 }
---
+++ src/components/Orders/Orders.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Orders = () => {
+    return (
+        <div>
+            <h2>hthis is ors</h2>
+        </div>
+    );
+};
+
+export default Orders;
