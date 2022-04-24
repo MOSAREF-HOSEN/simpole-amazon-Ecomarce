8c4a84628f8a468da95077c801e267a73d2ef558
Golap-mostofa <mostofa6410@gmail.com>
2022-04-24 23:42:35 +0600

    login and sign in implement

--- src/components/Anthontication/Login/SignUp/SignUp.js
+++ src/components/Anthontication/Login/SignUp/SignUp.js
@@ -1,52 +1,74 @@
 import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
-import './SignUp.css'
+import { Link, useNavigate } from 'react-router-dom';
+import { useCreateUserWithEmailAndPassword } from 'react-firebase-hooks/auth';
+import auth from '../../../../firebase.init';
+
+
 const SignUp = () => {
-    const [email,setEmail] = useState('')
-    const [password,setPassword] = useState('')
-    const [conpassword,setConpassword] = useState('')
-    const [ error,setError] = useState('')
-    const hendeleEmail = e =>{
-        setEmail(e.target.value)
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [confirmPassword, setConfirmPassword] = useState('');
+    const [error, setError] = useState('');
+    const navigate = useNavigate();
+
+    const [createUserWithEmailAndPassword, user] = useCreateUserWithEmailAndPassword(auth)
+
+    const handleEmailBlur = event =>{
+        setEmail(event.target.value);
     }
-    const hendelePassword = e =>{
-        setPassword(e.target.value)
+
+    const handlePasswordBlur = event =>{
+        setPassword(event.target.value);
     }
-    const hendeleConpassword = e =>{
-        setConpassword(e.target.value)
+
+    const handleConfirmPasswordBlur = event =>{
+        setConfirmPassword(event.target.value);
     }
-    const handleCreateUser = e =>{
-        e.preventDefault()
-        if(password !== conpassword){
-            setError('password mast be 6 carectars')
+
+    if(user){
+        navigate('/shop');
+        console.log('login');
+    }
+
+    const handleCreateUser = event =>{
+        event.preventDefault();
+        if(password !== confirmPassword){
+            setError('Your two passwords did not match');
+            return;
         }
+        if(password.length <6){
+            setError('Password must be 6 characters or longer');
+            return;
+        }
+        
+        createUserWithEmailAndPassword(email, password);
     }
+
     return (
         <div className='form-container'>
-        <div>
-            <h2 className='form-title'>Sign Up</h2>
-            <form onSubmit={handleCreateUser}>
-                <div className="input-group">
-                    <label htmlFor="email">Email</label>
-                    <input onBlur={hendeleEmail} type="email" name="email" id="" required/>
-                </div>
-                <div className="input-group">
-                    <label htmlFor="password">password</label>
-                    <input onBlur={hendelePassword} type="password" name="password" id="" required/>
-                </div>
-                <div className="input-group">
-                        <label htmlFor="Confirm password">Confirm password</label>
-                        <input onBlur={hendeleConpassword} type="password" name="Confirm password" id="" required/>
+            <div>
+                <h2 className='form-title'>Sign Up</h2>
+                <form onSubmit={handleCreateUser}>
+                    <div className="input-group">
+                        <label htmlFor="email">Email</label>
+                        <input onBlur={handleEmailBlur} type="email" name="email" id="" required/>
                     </div>
-                    <p style={{color:'red'}}>{error}</p>
-                <input className='form-submit' type="submit" value="Sign Up" />
-            </form>
-            <p>
-                Already have an account? <Link className='form-link' to='/login'>Login</Link>
-
-            </p>
+                    <div className="input-group">
+                        <label htmlFor="password">Password</label>
+                        <input onBlur={handlePasswordBlur} type="password" name="password" id=""  required/>
+                    </div>
+                    <div className="input-group">
+                        <label htmlFor="confirm-password">Confirm Password</label>
+                        <input onBlur={handleConfirmPasswordBlur} type="password" name="confirm-password" id="" />
+                    </div>
+                    <p style={{color: 'red'}}>{error}</p>
+                    <input className='form-submit' type="submit" value="Sign Up"  required/>
+                </form>
+                <p>
+                    Already Have an account? <Link className='form-link' to="/login">Login</Link>
+                </p>
+            </div>
         </div>
-    </div>
     );
 };
 
--- src/firebase.init.js
+++ src/firebase.init.js
@@ -1,3 +1,23 @@
+// // Import the functions you need from the SDKs you need
+// import { initializeApp } from "firebase/app";
+// import {getAuth} from 'firebase/auth'
+// // TODO: Add SDKs for Firebase products that you want to use
+// // https://firebase.google.com/docs/web/setup#available-libraries
+
+// // Your web app's Firebase configuration
+// const firebaseConfig = {
+//   apiKey: "AIzaSyCbNRveqK8EPjLrTQG-HDCeDgki6rg6Tc8",
+//   authDomain: "emazone-full-project.firebaseapp.com",
+//   projectId: "emazone-full-project",
+//   storageBucket: "emazone-full-project.appspot.com",
+//   messagingSenderId: "594010993984",
+//   appId: "1:594010993984:web:4701c9c293d8b38a9c76a5"
+// };
+
+// // Initialize Firebase
+// const app = initializeApp(firebaseConfig);
+// const auth = getAuth(app);
+// export default auth;
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import {getAuth} from 'firebase/auth'
@@ -6,12 +26,12 @@ import {getAuth} from 'firebase/auth'
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyCbNRveqK8EPjLrTQG-HDCeDgki6rg6Tc8",
-  authDomain: "emazone-full-project.firebaseapp.com",
-  projectId: "emazone-full-project",
-  storageBucket: "emazone-full-project.appspot.com",
-  messagingSenderId: "594010993984",
-  appId: "1:594010993984:web:4701c9c293d8b38a9c76a5"
+  apiKey: "AIzaSyA_NPlkC9nu2kOk1dxge6TyGOqwzP8kick",
+  authDomain: "emazone-with-fairbase.firebaseapp.com",
+  projectId: "emazone-with-fairbase",
+  storageBucket: "emazone-with-fairbase.appspot.com",
+  messagingSenderId: "778157612195",
+  appId: "1:778157612195:web:238166189d4d1e0376f316"
 };
 
 // Initialize Firebase
